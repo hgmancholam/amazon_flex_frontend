958e267ce39625cfed848cf7e39a2afdce398860
hgmancholam <hgmancholam@gmail.com>
2023-10-16 00:59:29 -0700

    ok

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,24 @@ import { Inter } from "next/font/google";
 import MainLayout from "./main-component";
 
 export const metadata = {
-  title: "EasyFlex",
-  description: "Easyflex",
+  title: "BpdFlex",
+  description: "BpdFlex",
+  applicationName: "BpdFlex",
+  viewport:
+    "width=device-width, initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=0,shrink-to-fit=1",
+  keywords: [],
+  openGraph: {
+    type: "website",
+    url: "https://bpdflex.com",
+    title: "BpdFlex",
+    description: "BpdFlex",
+    siteName: "BpdFlex",
+    images: [
+      {
+        url: "https://bpdflex.com/logo_square.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
